2cfe03a1d7d875002dd3a6d32bec623a9cdf7516
Matias Luna <mtsluna@gmail.com>
2024-04-05 23:49:40 -0300

    fix: some fixes with test

--- src/app/services/product/product.service.ts
+++ src/app/services/product/product.service.ts
@@ -3,6 +3,7 @@ import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import backendConstants from "../../constants/backend-constants";
 import {Product} from "../../interfaces/product";
+import {map} from "rxjs/operators";
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,15 @@ export class ProductService {
   constructor(private httpClient: HttpClient) { }
 
   getProduct(menuId: string, productId: string): Observable<Product> {
-    return this.httpClient.get<Product>(`${backendConstants.baseUrl}/products/${productId}`);
+    return this.httpClient.get<Product>(`${backendConstants.baseUrl}/products/${productId}`)
+      .pipe(
+        map((product) => {
+          product.customizations?.forEach((customization) => {
+            customization.options.sort((a, b) => a.price.amount - b.price.amount)
+          })
+
+          return product;
+        })
+      );
   }
 }
