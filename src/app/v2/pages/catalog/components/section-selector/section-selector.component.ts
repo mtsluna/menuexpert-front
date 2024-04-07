1500be9f709ecbcf4e8f2db7d443a3696ad8aa26
Matias Luna <mtsluna@gmail.com>
2024-04-07 20:30:30 -0300

    feat: provide intelligence to section selecto, auto category picker

--- src/app/v2/pages/catalog/components/section-selector/section-selector.component.ts
+++ src/app/v2/pages/catalog/components/section-selector/section-selector.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnInit} from '@angular/core';
+import {Component, HostListener, Input, OnChanges, OnInit, SimpleChanges} from '@angular/core';
 import {Category} from "../../../../../interfaces/category";
 
 @Component({
@@ -6,10 +6,11 @@ import {Category} from "../../../../../interfaces/category";
   templateUrl: './section-selector.component.html',
   styleUrls: ['./section-selector.component.scss']
 })
-export class SectionSelectorComponent implements OnInit {
+export class SectionSelectorComponent implements OnInit, OnChanges {
 
   @Input()
   sections: Array<Category> | undefined;
+  sectionsPosition: { id: string, yValue: number }[] = [];
   selected: string = '';
 
   constructor() { }
@@ -20,13 +21,38 @@ export class SectionSelectorComponent implements OnInit {
     }
   }
 
+  ngOnChanges(changes: SimpleChanges) {
+    this.sectionsPosition = (this.sections?.map((category) => ({
+      id: category.id,
+      yValue: document.getElementById(category.id)?.getClientRects()[0].y || 0
+    }))) || []
+  }
+
   onClick(id: string) {
-    this.selected = id;
     const elem = document.getElementById(id);
 
-    elem?.scrollIntoView({
-      behavior: 'smooth',
-    });
+    elem?.scrollIntoView();
+  }
+
+  @HostListener('window:scroll', ['$event'])
+  detectPosition() {
+    this.sectionsPosition = (this.sections?.map((category) => ({
+      id: category.id,
+      yValue: window.scrollY + (document.getElementById(category.id)?.getBoundingClientRect().y || 0)
+    }))) || [];
+
+    this.sectionsPosition.sort((a, b) => a.yValue - b.yValue)
+
+
+    let [ actual] = this.sectionsPosition
+      .filter((elem) => elem.yValue - 170 < window.scrollY)
+      .slice(-1);
+
+    if(!actual) {
+      [ actual ] = this.sectionsPosition.slice(0, 1);
+    }
+
+    this.selected = actual.id || ''
   }
 
 }
