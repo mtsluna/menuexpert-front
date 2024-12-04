8cd7d43aeb6a9d28e99b7e4bfb252447b6b03579
mtsluna <mtsluna@gmail.com>
2024-12-03 23:18:04 -0300

    beautify google sign in

--- src/app/components/shared/navbar/navbar.component.ts
+++ src/app/components/shared/navbar/navbar.component.ts
@@ -1,100 +1,47 @@
 import {AfterViewInit, Component, OnInit} from '@angular/core';
 import {CartService} from "../../../services/cart.service";
-import {AuthService} from "@auth0/auth0-angular";
-import {SocialAuthService} from "@abacritt/angularx-social-login";
+import {SocialAuthService, SocialUser} from "@abacritt/angularx-social-login";
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
   styleUrls: ['./navbar.component.scss']
 })
-export class NavbarComponent implements OnInit, AfterViewInit {
+export class NavbarComponent implements OnInit {
 
   isAuthenticated: boolean = false;
-  user: any | undefined;
+  user: SocialUser | undefined;
   image: string | undefined;
-  loading: boolean = true;
+  loggedIn: boolean = false;
 
   constructor(
-    // public authService: AuthService,
     private cartService: CartService,
     private authService: SocialAuthService
-  ) { }
+  ) {
+
+    console.log(this.user)
+    if(!this.user) {
+
+      console.log('No user found');
+
+      this.user = JSON.parse(localStorage.getItem('user') || '')
+
+      console.log(this.user)
+    }
+
+  }
 
   async ngOnInit(): Promise<void> {
-    // this.authService.user$.subscribe({
-    //   next: (data) => {
-    //     console.log(data)
-    //     this.user = data;
-    //     this.image = data?.picture
-    //     this.loading = false;
-    //   }
-    // })
-    this.authService.authState.subscribe({
-      next: (data) => {
-        console.log(data)
-      }
-    })
-  }
-
-  ngAfterViewInit(): void {
-    this.loadGoogleLogin();
-  }
-
-  loadButtons() {
-    console.log("load")
-    this.loadGoogleLogin();
-  }
-
-  loadGoogleLogin() {
-    if (window.google && window.google.accounts && window.google.accounts.id) {
-      // Inicializar el widget manualmente
-      window.google.accounts.id.initialize({
-        client_id: '52106838606-nepv3nvq93uctr0bglmna5mik69bnfug.apps.googleusercontent.com',
-        callback: this.handleCredentialResponse,
-        auto_select: false,
-        login_uri: 'https://youtube.com',
-      });
-      // Renderizar el botón
-      window.google.accounts.id.renderButton(
-        document.querySelector('.g_id_signin') as HTMLElement,
-        {
-          type: 'standard',
-          size: 'large',
-          theme: 'outline',
-          text: 'signin',
-          shape: 'rectangular',
-          logo_alignment: 'left',
-        }
-      );
-    }
-  }
-
-  handleCredentialResponse(response: any): void {
-    console.log('Credential Response:', response);
-
-    this.isAuthenticated = !!(response && response.credential);
-  }
-
-  async login() {
-    // this.authService.loginWithRedirect({
-    //   authorizationParams: {
-    //     screen_hint: 'login',
-    //     prompt: 'select_account',
-    //     display: 'page',
-    //   }
-    // })
+    this.authService.authState.subscribe((user) => {
+      this.user = user;
+      this.loggedIn = (user != null);
+      localStorage.setItem('user', JSON.stringify(user))
+    });
   }
 
   async logout() {
-    // this.authService.logout({
-    //   logoutParams: {
-    //     returnTo: window.location.origin + '/qr',
-    //   }
-    // });
+    await this.authService.signOut()
     this.cartService.clearCart();
     await this.cartService.getCartId();
   }
-
-  protected readonly document = document;
 }
